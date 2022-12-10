836c85fb7ef631ab5311a57fe11b3373e8750b0c
Olga Brazhkina <emelyanova84@yandex.ru>
2022-12-10 11:32:05 +0500

    Create classes Post, Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post ();
+        post.name = "Иван";
+        post.passport = "1234 № 123456";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Петров";
+        post.subscription = true;
+    }
+}
