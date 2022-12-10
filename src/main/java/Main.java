030b3942933a78df3fac09ee45d189b30cbf640b
Olga Brazhkina <emelyanova84@yandex.ru>
2022-12-10 11:50:06 +0500

    Task done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Post post = new Post ();
+        Post post = new Post();
         post.name = "Иван";
         post.passport = "1234 № 123456";
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Петров";
         post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 31;
+        post.birthday.month = 12;
+        post.birthday.year = 2022;
     }
 }
